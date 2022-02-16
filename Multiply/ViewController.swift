a96852c6de98890c7b4b3f247fe2e7c6256ee834
Edwin Gonzalez <edwincanelogh8@icloud.com>
2022-02-16 14:10:45 -0700

    add in more assets

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var images: UIImageView!
+    
+    @IBOutlet weak var Firstnumber: UITextField!
+    @IBOutlet weak var Secondnumber: UITextField!
+    @IBOutlet weak var Answer: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    
+    @IBAction func switchbutton(_ sender: Any) {
+    }
 }
 
